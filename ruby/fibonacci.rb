e5ce9ddadc568ad7b3f817e9c94dcb507e78864d
Joshua Holmes <joshua.phillip.holmes@gmail.com>
2022-01-11 21:47:31 -0600

    Completed assignment

--- ruby/fibonacci.rb
+++ ruby/fibonacci.rb
@@ -1,5 +1,10 @@
 def fibonacci(num)
-  # type your code in here
+  fibArr = []
+  for i in 0..num do
+    lastTwo = fibArr[-2, 2]
+    lastTwo ? fibArr.push(lastTwo.sum) : fibArr.push(i)
+  end
+  fibArr.last
 end
 
 if __FILE__ == $PROGRAM_NAME
@@ -17,7 +22,25 @@ if __FILE__ == $PROGRAM_NAME
   puts "=>", fibonacci(10)
 
   # Don't forget to add your own!
+
+  puts
+
+  puts "Expecting 8"
+  puts "=>", fibonacci(6)
 end
 
 # Please add your pseudocode to this file
 # And a written explanation of your solution
+=begin 
+
+set my fib numbers to empty array []
+n number of times, do this
+    if i < 2
+      then push i to fib numbers
+    else
+      push the sum of the last two fib numbers to fib numbers
+      
+return the last fib number
+
+
+=end
